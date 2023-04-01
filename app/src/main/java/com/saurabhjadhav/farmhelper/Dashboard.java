55486c7a0910807f9b0d589776c9aaf3450b2401
SAURABHJADHAV.CSE <saurabhjadhav.cse@gmail.com>
2023-04-01 21:25:31 +0530

    ChatGptApi

--- app/src/main/java/com/saurabhjadhav/farmhelper/Dashboard.java
+++ app/src/main/java/com/saurabhjadhav/farmhelper/Dashboard.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckedTextView;
 import android.widget.LinearLayout;
 
+import com.saurabhjadhav.farmhelper.ChatGptHelper.ChatGptHelper;
 import com.saurabhjadhav.farmhelper.DiseasesPrediction.DiseasesPredictor;
 
 public class Dashboard extends AppCompatActivity {
 
-    LinearLayout PlantDiseasesPredictor;
+    LinearLayout PlantDiseasesPredictor, ChatBot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,19 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         PlantDiseasesPredictor = findViewById(R.id.PlantDieasesPrediction);
+        ChatBot = findViewById(R.id.ChatBot);
 
         PlantDiseasesPredictor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Dashboard.this, DiseasesPredictor.class));
+            }
+        });
+
+        ChatBot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this, ChatGptHelper.class));
             }
         });
 
