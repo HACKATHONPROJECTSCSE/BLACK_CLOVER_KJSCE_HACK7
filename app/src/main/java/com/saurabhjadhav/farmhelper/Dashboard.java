9ba4c62f104407fa537e86a6221c4ef55ff61b88
SAURABHJADHAV.CSE <saurabhjadhav.cse@gmail.com>
2023-04-01 19:58:17 +0530

    DashboardComplete Commit + Added Dieases Predictor Module UI + Backend

--- app/src/main/java/com/saurabhjadhav/farmhelper/Dashboard.java
+++ app/src/main/java/com/saurabhjadhav/farmhelper/Dashboard.java
@@ -2,15 +2,30 @@ package com.saurabhjadhav.farmhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.saurabhjadhav.farmhelper.DiseasesPrediction.DiseasesPredictor;
 
 public class Dashboard extends AppCompatActivity {
+
+    LinearLayout PlantDiseasesPredictor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        PlantDiseasesPredictor = findViewById(R.id.PlantDieasesPrediction);
+
+        PlantDiseasesPredictor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this, DiseasesPredictor.class));
+            }
+        });
 
     }
 }
