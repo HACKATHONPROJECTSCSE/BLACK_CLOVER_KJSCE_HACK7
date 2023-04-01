0c43f7d33a0cb47bfc7d6f3734bbccadf83f6f19
SAURABHJADHAV.CSE <saurabhjadhav.cse@gmail.com>
2023-04-02 02:02:10 +0530

    Small Commits

--- app/src/main/java/com/saurabhjadhav/farmhelper/Dashboard.java
+++ app/src/main/java/com/saurabhjadhav/farmhelper/Dashboard.java
@@ -9,12 +9,13 @@ import android.widget.CheckedTextView;
 import android.widget.LinearLayout;
 
 import com.saurabhjadhav.farmhelper.ChatGptHelper.ChatGptHelper;
+import com.saurabhjadhav.farmhelper.Contacts.DashboardContact;
 import com.saurabhjadhav.farmhelper.DiseasesPrediction.DiseasesPredictor;
 import com.saurabhjadhav.farmhelper.Weather.WeatherActivity;
 
 public class Dashboard extends AppCompatActivity {
 
-    LinearLayout PlantDiseasesPredictor, Weather, ChatBot;
+    LinearLayout PlantDiseasesPredictor, Weather, ChatBot, Contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,7 @@ public class Dashboard extends AppCompatActivity {
         PlantDiseasesPredictor = findViewById(R.id.PlantDieasesPrediction);
         ChatBot = findViewById(R.id.ChatBot);
         Weather = findViewById(R.id.Weather);
+        Contact = findViewById(R.id.Contact);
 
         PlantDiseasesPredictor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +45,13 @@ public class Dashboard extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Dashboard.this, WeatherActivity.class));
+            }
+        });
+
+        Contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this, DashboardContact.class));
             }
         });
 
