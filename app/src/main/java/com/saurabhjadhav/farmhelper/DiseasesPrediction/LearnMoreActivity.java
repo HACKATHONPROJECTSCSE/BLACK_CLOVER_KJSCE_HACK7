c819a5c9edde44d8fd4422c3734d90eff8750b6f
omkokate <om.kokate2902@gmial.com>
2023-04-02 05:09:35 +0530

    Last Commit Day 1 -om

--- app/src/main/java/com/saurabhjadhav/farmhelper/DiseasesPrediction/LearnMoreActivity.java
+++ app/src/main/java/com/saurabhjadhav/farmhelper/DiseasesPrediction/LearnMoreActivity.java
@@ -4,17 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.saurabhjadhav.farmhelper.MainActivity;
 import com.saurabhjadhav.farmhelper.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LearnMoreActivity extends AppCompatActivity {
 
@@ -29,8 +31,8 @@ public class LearnMoreActivity extends AppCompatActivity {
         TextView disease_name;
         TextView disease_description;
 
-        disease_name=findViewById(R.id.disease_name);
-        disease_description=findViewById(R.id.disease_description);
+        disease_name = findViewById(R.id.disease_name);
+        disease_description = findViewById(R.id.disease_description);
 
 
         disease_name.setText(disease);
@@ -39,23 +41,26 @@ public class LearnMoreActivity extends AppCompatActivity {
         String url = "https://web-production-226d.up.railway.app/wiki-summary?query=";
         url = url + disease_name;
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-//                        Toast.makeText(LearnMoreActivity.this, response, Toast.LENGTH_SHORT).show();
-                        disease_description.setText(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(LearnMoreActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(stringRequest);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
 
+                    String result = response.getString("result");
+                    disease_description.setText(result);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("myapp", "onErrorResponse: " + error.getMessage());
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
-}
+    }
